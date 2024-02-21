3f66c733190614df30acf4f526a965f8ed420bad
Gustavo Macedo Vieira <158500964+Gustavo3522@users.noreply.github.com>
2024-02-21 15:28:10 -0300

    Update index.py

--- EX35/index.py
+++ EX35/index.py
@@ -5,5 +5,4 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print(' Os segmentos acima podem formar um triangulo!')
 else:
     print('Os segmentos acima não podem formar um triangulo!')
-
-print('\033[4;30;45m Óla mundo\033a[m')  
+    
